357067c2233a09bd8cd98b67bdf98d53874de906
Olivia Cunningham <Liv.cunninghamgb@gmail.com>
2019-06-04 09:17:01 -0400

    yesterdays homework

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Slider from './components/Slider'
 
 class App extends Component {
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <Slider />
+      </>
+    )
   }
 }
 
---
+++ src/components/Slider.js
@@ -0,0 +1,89 @@
+import React, { useState } from 'react'
+
+export default function Slider() {
+  const [hue, setHue] = useState(Math.floor(Math.random(setHue) * 320))
+  const [saturation, setSaturation] = useState(
+    Math.floor(Math.random(setSaturation) * 100)
+  )
+  const [lightness, setLightness] = useState(
+    Math.floor(Math.random(setLightness) * 100)
+  )
+  const [alpha, setAlpha] = useState(Math.floor(Math.random(setAlpha) * 100))
+
+  const getRandom = () => {
+    setHue(Math.floor(Math.random() * 320))
+    setSaturation(Math.floor(Math.random() * 100))
+    setLightness(Math.floor(Math.random() * 100))
+    setAlpha(Math.floor(Math.random() * 100))
+  }
+
+  return (
+    <>
+      <section
+        className="colors-yo"
+        style={{
+          backgroundColor: `hsl(${hue}, 
+            ${saturation}%,
+            ${lightness}%,
+            ${alpha}%
+            )`
+        }}
+      >
+        <span className="colorme">COLOR ME</span>
+      </section>
+
+      <section className="sliders">
+        <div>
+          <input
+            className="H"
+            type="range"
+            min="0"
+            max="320"
+            value={setHue}
+            onChange={e => setHue(e.target.value)}
+          />
+          <h1>HUE</h1>
+        </div>
+
+        <div>
+          <input
+            className="S"
+            type="range"
+            min="0"
+            max="100"
+            value="setSaturation"
+            onChange={e => setSaturation(e.target.value)}
+          />
+          <h1>SATURATION</h1>
+        </div>
+
+        <div>
+          <input
+            className="L"
+            type="range"
+            min="0"
+            max="100"
+            value="setLightness"
+            onChange={e => setLightness(e.target.value)}
+          />
+          <h1>LIGHTNESS</h1>
+        </div>
+
+        <div>
+          <input
+            className="A"
+            type="range"
+            min="0"
+            max="100"
+            value="setAlpha"
+            onChange={e => setAlpha(e.target.value)}
+          />
+          <h1>ALPHA</h1>
+        </div>
+        <button onClick={getRandom}>
+          <span role="img">🌈</span>
+        </button>
+      </section>
+    </>
+  )
+}
